5ca519eafbfaae600559f916ca8a3230831e76d0
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-25 12:50:16 -0700

    add local testing passwords to config/connection files

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: 'localhost',
+  user: 'root',
+  password: 'Passw0rd',
+  database: 'burgers_db'
 });
 
-
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
