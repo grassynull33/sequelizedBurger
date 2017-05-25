40ef2bf0b8ce00a4dbda28baa4401c16965ad561
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-25 13:36:03 -0700

    remove force true in server.js to prevent nuking

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var PORT = process.env.PORT || 3000;
 // app.listen(PORT);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
   });
