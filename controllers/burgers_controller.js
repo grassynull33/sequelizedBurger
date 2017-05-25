fa38fe7f53af152253a61ad8ff4bbb707457044f
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-25 13:33:02 -0700

    change orm methods to sequelize methods in burgers_controller, change burger model to sequelize Burger model, and sync in server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,41 +1,57 @@
-var express = require("express");
+var express = require('express');
 
 var router = express.Router();
-var burger = require("../models/burger");
+// var burger = require('../models/burger');
+
+// Requiring our Burger model
+var db = require('../models');
 
 // get route -> index
-router.get("/", function(req, res) {
-  res.redirect("/burgers");
+router.get('/', function (req, res) {
+  res.redirect('/burgers');
 });
 
-router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(data) {
-    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-    var hbsObject = { burgers: data };
-    res.render("index", hbsObject);
+router.get('/burgers', function (req, res) {
+  db.Burger.findAll({})
+  .then(function (dbBurger) {
+    var data = {
+      burgers: []
+    };
+
+    console.log(dbBurger);
+
+    for (var i = 0; i < dbBurger.length; i++) {
+      data.burgers.push(dbBurger[i].dataValues);
+    }
+
+    res.render('index', data);
   });
 });
 
 // post route -> back to index
-router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for buger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+router.post('/burgers/create', function (req, res) {
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  })
+    .then(function (dbBurger) {
+      res.redirect('/');
+    });
 });
 
 // put route -> back to index
-router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+router.put('/burgers/update', function (req, res) {
+  db.Burger.update(
+    {
+      devoured: true
+    },
+    {
+      where: {
+        id: req.body.burger_id
+      }
+    })
+    .then(function (dbBurger) {
+      res.redirect('/');
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,40 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// Requiring our models for syncing
+var db = require('./models');
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 // override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-var exphbs = require("express-handlebars");
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({
-  defaultLayout: "main"
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
 }));
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
-var routes = require("./controllers/burgers_controller");
+var routes = require('./controllers/burgers_controller');
 
-app.use("/", routes);
-app.use("/update", routes);
-app.use("/create", routes);
+app.use('/', routes);
+app.use('/update', routes);
+app.use('/create', routes);
 
 // listen on port 3000
-var port = process.env.PORT || 3000;
-app.listen(port);
+var PORT = process.env.PORT || 3000;
+// app.listen(PORT);
+
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
+  });
+});
